d6f34355efa40a778f6387a50756de3464c12a99
PUIPUI-MOLCAR <ino.bigdragons.masa@gmail.com>
2021-02-04 21:01:06 +0900

    add abc086-A

---
+++ abc/086/a.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+const long long INF = 1LL << 60;
+
+int main(){
+  int a,b;
+  cin >> a >> b;
+  
+  if(a%2 == 0 || b%2 == 0){
+    cout << "Even" << endl;
+  } else {
+    cout << "Odd" << endl;
+  }
+  return 0;
+}
